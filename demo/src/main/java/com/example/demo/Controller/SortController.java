b33cc1c086e7f477ead46ae9646cf54483aa1ee2
Usiel-Curt <20243ds143@utez.edu.mx>
2025-11-25 08:52:58 -0600

    U2_TI_24_SortController

---
+++ demo/src/main/java/com/example/demo/Controller/SortController.java
@@ -0,0 +1,40 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.RequestCadena;
+import com.example.demo.service.SortService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController //Configuracion del controlador (por tag)
+@RequestMapping("/api/sort")//configuracion del endpoint de la API
+public class SortController {
+      @Autowired
+        SortService service;
+
+     @PostMapping("/burbuja")
+     public ResponseEntity<?> bubbleSort(@RequestBody RequestCadena request) {
+        String [] partes = request.getCadena().split(",");//recibimos el string y lo convertimos en 
+        int [] numeros = new int [partes.length];//declaracion de el arreglo numeros
+        for (int i = 0; i < partes.length; i++) {
+           try{
+            numeros[i] = Integer.parseInt(partes[i]);
+
+           }catch(Exception e){
+                numeros[i] = 0;
+           }
+            
+        }
+
+        return ResponseEntity.ok(service.sort(numeros));
+        
+    }
+    
+    
+}
